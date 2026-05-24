552bb00454a68513e3d33ab1df5feacc19845230
yatin.sharma <yatin.srma@gmail.com>
2026-05-24 19:11:11 +0530

    feat(neovim): make neovim transparent

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -1,11 +1,28 @@
 return {
-  "catppuccin/nvim", 
-    name = "catppuccin", 
+  "catppuccin/nvim",
+    name = "catppuccin",
     priority = 1000,
 
     config = function()
+        local function transparent()
+            vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+            vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+            vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+            vim.api.nvim_set_hl(0, "NeoTreeNormal", { bg = "none" })
+            vim.api.nvim_set_hl(0, "NeoTreeNormalNC", { bg = "none" })
+            --vim.api.nvim_set_hl(0, "NeoTreeWinSeparator", { bg = "none" }) -- Optional: makes the sidebar border line transparent too
+        end
+
+
+        vim.api.nvim_create_autocmd("ColorScheme", {
+            pattern = "*",
+            callback = transparent,
+        })
+
+
         vim.cmd.colorscheme "catppuccin-mocha"
     end
+
 }
 
 --return{
